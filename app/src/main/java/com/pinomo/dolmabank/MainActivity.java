214d1056d3d46d7a42ba52d9c841096ac3ecd2e5
SigmaMale <162889753+Selezen8172@users.noreply.github.com>
2024-03-31 22:34:01 +0400

    Add files via upload

--- app/src/main/java/com/pinomo/dolmabank/MainActivity.java
+++ app/src/main/java/com/pinomo/dolmabank/MainActivity.java
@@ -1,51 +1,31 @@
-package com.pinomo.dolmabank;
+package com.example.dolmabank;
 
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowInsetsController;
-import android.view.WindowManager;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
-
-import com.pinomo.dolmabank.databinding.ActivityMainBinding;
-
-import java.util.Objects;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
-    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        if (savedInstanceState == null){
-            NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-            assert navHostFragment != null;
-            navController = navHostFragment.getNavController();
-            // navController.navigate(R.id.action_loadingFragment_to_loginFragment);
-        }
-
-        customizeStatusBarAndTheme();
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
-
-    private void customizeStatusBarAndTheme() {
-        getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-        );
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            Objects.requireNonNull(getWindow().getInsetsController()).setSystemBarsAppearance(
-                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
-                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
-            );
-        } else {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
+    public void startNewActivity(View v){
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
     }
 }
